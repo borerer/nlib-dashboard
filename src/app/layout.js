45102e76950df6df0e7d56e74973c5b71d3dfe1c
Li Zhao <iloahz@gmail.com>
2023-04-10 01:32:53 +0800

    update logs style

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@
 import './globals.css';
 import Nav from './nav';
 import React from 'react';
+import "@fontsource/roboto-mono";
 
 export default function RootLayout({ children }) {
   const [page, setPage] = React.useState('apps');
--- src/app/logs/page.js
+++ src/app/logs/page.js
@@ -1,35 +1,30 @@
 'use client';
 
 import React, { useEffect } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-// import './LogPage.css';
+import './styles.css';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import DeleteIcon from '@mui/icons-material/Delete';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import CheckIcon from '@mui/icons-material/Check';
-import SaveIcon from '@mui/icons-material/Save';
-import { TextField } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import { toast } from 'react-toastify';
 import Chip from '@mui/material/Chip';
 import axios from 'axios';
+import Tooltip from '@mui/material/Tooltip';
 
 dayjs.extend(relativeTime);
 axios.defaults.baseURL = 'https://nlib.home.iloahz.com';
 
+const levelToColorMap = {
+  'debug': 'default',
+  'info': 'info',
+  'warn': 'warning',
+  'error': 'error',
+}
+
 export default function Logs() {
 
   const [logs, setLogs] = React.useState([]);
+  const [useFromNow, setUseFromNow] = React.useState(true);
 
-
-  const getLogs = async () => {
-    const res = await axios.get('/api/app/logs/get');
+  const getLogs = async (limit, skip) => {
+    const res = await axios.get('/api/app/logs/get', {params: {n: limit, skip: skip}});
     return res.data;
   }
 
@@ -42,14 +37,59 @@ export default function Logs() {
     updateLogs();
   }, [setLogs]);
 
+  const levelToColor = (level) => {
+    if (levelToColorMap[level] != null) {
+      return levelToColorMap[level];
+    }
+    return 'default';
+  }
+
+  const paddingAround = (s, len) => {
+    const left = Math.floor((len - s.length) / 2);
+    return s.padStart(s.length + left).padEnd(len);
+  }
+
+  const timestamp = (t) => {
+    const date = dayjs(t);
+    if (useFromNow) {
+      return <div className='timestamp' onClick={() => setUseFromNow(false)}>
+        <span>
+          {`[ ${paddingAround(date.fromNow(), 21)} ]`}
+        </span>
+      </div>
+    } else {
+      return <div className='timestamp' onClick={() => setUseFromNow(true)}>
+        <span>
+          {`[ ${date.format('YY-MM-DD HH:mm:ss.SSS')} ]`}
+        </span>
+      </div>
+    }
+  }
+
   return (
-    <div className="logs">
+    <div className='logs'>
         {logs.map(log => {
-            const date = dayjs(log.timestamp);
-            return <div>
-              <Chip label={log.details['app_id']}></Chip>
-              <Chip label={date.fromNow()}></Chip>
-              <span>{log.message}</span>
+            return <div className={`log-line ${log.level}`}>
+              {timestamp(log.timestamp)}
+              <div className='app-id'>
+                <span>
+                  {`[ ${paddingAround(log.details['app_id'], 11)} ]`}
+                </span>
+                {/* <Chip label={log.details['app_id']} size='small'></Chip> */}
+              </div>
+              {/* <div className='level'>
+                <Chip label={log.level} size='small' color={levelToColor(log.level)}></Chip>
+              </div> */}
+              <div className='message'>
+                <span>{log.message}</span>
+              </div>
+              <div className='details'>
+                {Object.keys(log.details).filter(k => k != 'app_id').sort().map(k => {
+                  return <Tooltip title={k}>
+                    <Chip label={log.details[k]} size='small'></Chip>
+                  </Tooltip>
+                })}
+              </div>
             </div>
         })}
     </div>
